302ea45e1e686e3218b904443072e733c4f23de7
JC Grubbs <grubbs@GRUBBS-MBP.local>
2010-06-09 12:27:49 -0500

    stuff

--- data.js
+++ data.js
@@ -1,18 +1,18 @@
 var ganttData = [
 	{
-		id: 1, title: "Feature 1", series: [
+		id: 1, name: "Feature 1", series: [
 			{ name: "Planned", start: new Date(2010,01,01), end: new Date(2010,01,05) },
 			{ name: "Actual", start: new Date(2010,01,01), end: new Date(2010,01,05) }
 		]
 	},
 	{
-		id: 2, title: "Feature 2", series: [
+		id: 2, name: "Feature 2", series: [
 			{ name: "Planned", start: new Date(2010,01,01), end: new Date(2010,01,05) },
 			{ name: "Actual", start: new Date(2010,01,01), end: new Date(2010,01,05) }
 		]
 	},
 	{
-		id: 3, title: "Feature 3", series: [
+		id: 3, name: "Feature 3", series: [
 			{ name: "Planned", start: new Date(2010,01,01), end: new Date(2010,01,05) },
 			{ name: "Actual", start: new Date(2010,01,01), end: new Date(2010,01,05) }
 		]
--- jquery.ganttView.js
+++ jquery.ganttView.js
@@ -13,15 +13,15 @@ rollup: string (day, week, month)
 		var els = this;
 		var defaults = {};
 		var opts = $.extend(defaults, options);
-		var dates = Gantt.getDates(opts.start, opts.end);
-		var months = Gantt.getMonths(dates);
+		var months = Gantt.getMonths(opts.start, opts.end);
 		
 		els.each(function () {
 			
 			var div = $(this);
 			var tbl = $("<table>", { "class": "ganttview" });
 			
-			Gantt.addHeader(tbl, months, dates);
+			Gantt.addHeaderToTable(tbl, months);
+			Gantt.addGridToTable(tbl, opts.data, months);
 			
 			div.append(tbl);
 		});
@@ -29,37 +29,72 @@ rollup: string (day, week, month)
 	
 	var Gantt = {
 		monthNames: ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
-		addHeader: function (tbl, months, dates) {
+		getMonths: function (start, end) {
+			var months = []; months[start.getMonth()] = [start];
+			var last = start;
+			while (last.compareTo(end) == -1) {
+				var next = last.clone().addDays(1);
+				if (!months[next.getMonth()]) { months[next.getMonth()] = []; }
+				months[next.getMonth()].push(next);
+				last = next;
+			}
+			return months;
+		},
+		addHeaderToTable: function (tbl, months) {
 			var thead = $("<thead></thead>");
 			var monthTr = $("<tr></tr>");
 			var emptyHdr = "<th rowspan='2'>&nbsp;</th>";
 			monthTr.append(emptyHdr).append(emptyHdr);
-			for (var i = 0; i < months.length; i++) {
-				monthTr.append("<th colspan='3' class='ganttview-header-month'>" + Gantt.monthNames[months[i]] + "</th>");
+			var dateTr = $("<tr></tr>");
+			for (var i = 0; i < 12; i++) {
+				if (months[i]) {
+					monthTr.append("<th colspan='" + months[i].length + "' class='ganttview-header-month'>" + Gantt.monthNames[i] + "</th>");
+					for (var j = 0; j < months[i].length; j++) {
+						dateTr.append("<th class='ganttview-header-day'>" + months[i][j].getDate() + "</th>");
+					}
+				}
 			}
 			thead.append(monthTr);
-			var dateTr = $("<tr></tr>");
-			for (var i = 0; i < dates.length; i++) {
-				dateTr.append("<th class='ganttview-header-day'>" + dates[i].getDate() + "</th>");
-			}
 			thead.append(dateTr);
 			tbl.append(thead);
 		},
-		getDates: function (start, end) {
-			var dates = [start];
-			while (dates[dates.length-1].compareTo(end) == -1) {
-				dates.push(dates[dates.length-1].clone().addDays(1));
-			}
-			return dates;
-		},
-		getMonths: function (dates) {
-			var months = [dates[0].getMonth()];
-			for (var i = 0; i < dates.length; i++) {
-				if (!ArrayUtils.contains(months, dates[i].getMonth())) {
-					months.push(dates[i].getMonth());
+		addGridToTable: function (tbl, data, months) {
+			
+			var tbody = $("<tbody></tbody>");
+			
+			for (var i = 0; i < data.length; i++) {
+				
+				var tr = $("<tr></tr>");
+				var itemTd = $("<td class='ganttview-item-name' rowspan='" + data[i].series.length + "'>" + data[i].name + "</td>");
+				tr.append(itemTd);
+				
+				for (var j = 0; j < data[i].series.length; j++) {
+					var seriesTd = $("<td class='ganttview-series-name'>" + data[i].series[0].name + "</td>");
+					tr.append(seriesTd);
+					Gantt.addCellsToSeriesRow(tr, months);
+				}
+				
+				tbody.append(tr);
+				
+				for (var j = 1; j < data[i].series.length; j++) {
+					tr = $("<tr></tr>");
+					var seriesTd = $("<td class='ganttview-series-name'>" + data[i].series[j].name + "</td>");
+					tr.append(seriesTd);
+					Gantt.addCellsToSeriesRow(tr, months);
+					tbody.append(tr);
 				}
 			}
-			return months;
+			
+			tbl.append(tbody);
+		},
+		addCellsToSeriesRow: function (tr, months) {
+			for (var i = 0; i < 12; i++) {
+				if (months[i]) {
+					for (var j = 0; j < months[i].length; j++) {
+						tr.append("<td class='ganttview-series-cell'></td>");
+					}
+				}
+			}
 		}
 	};
 	
