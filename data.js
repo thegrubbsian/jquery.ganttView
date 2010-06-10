e3981af52d68fd892ff59a829a74eb0e46d59019
JC Grubbs <jc.grubbs@devmynd.com>
2010-06-10 00:11:10 -0500

    made some good progress...still some issues and i screwed up the markup it will need to be refactored before the grid will scroll

--- data.js
+++ data.js
@@ -1,27 +1,26 @@
 var ganttData = [
 	{
 		id: 1, name: "Feature 1", series: [
-			{ name: "Planned", start: new Date(2010,01,01), end: new Date(2010,01,05) },
-			{ name: "Actual", start: new Date(2010,01,01), end: new Date(2010,01,05) }
+			{ name: "Planned", start: new Date(2010,00,01), end: new Date(2010,00,03) },
+			{ name: "Actual", start: new Date(2010,00,02), end: new Date(2010,00,05) }
 		]
 	}, 
 	{
 		id: 2, name: "Feature 2", series: [
-			{ name: "Planned", start: new Date(2010,01,01), end: new Date(2010,01,05) },
-			{ name: "Actual", start: new Date(2010,01,01), end: new Date(2010,01,05) }
+			{ name: "Planned", start: new Date(2010,00,05), end: new Date(2010,00,20) },
+			{ name: "Actual", start: new Date(2010,00,06), end: new Date(2010,00,17) }
 		]
 	}, 
 	{
 		id: 3, name: "Feature 3", series: [
-			{ name: "Planned", start: new Date(2010,01,01), end: new Date(2010,01,05) },
-			{ name: "Actual", start: new Date(2010,01,01), end: new Date(2010,01,05) }
+			{ name: "Planned", start: new Date(2010,00,11), end: new Date(2010,01,03) },
+			{ name: "Actual", start: new Date(2010,00,15), end: new Date(2010,01,03) }
 		]
 	}, 
 	{
 		id: 4, name: "Feature 4", series: [
-			{ name: "Planned", start: new Date(2010,01,01), end: new Date(2010,01,05) },
-			{ name: "Actual", start: new Date(2010,01,01), end: new Date(2010,01,05) },
-			{ name: "Projected", start: new Date(2010,01,01), end: new Date(2010,01,05) }
+			{ name: "Planned", start: new Date(2010,01,01), end: new Date(2010,01,03) },
+			{ name: "Actual", start: new Date(2010,01,01), end: new Date(2010,01,05) }
 		]
 	}
 ];
--- jquery.ganttView.js
+++ jquery.ganttView.js
@@ -11,7 +11,7 @@ cellWidth: number
     jQuery.fn.ganttView = function (options) {
 		
 		var els = this;
-		var defaults = { cellWidth: 21 };
+		var defaults = { cellWidth: 21, vHeaderWidth: 100 };
 		var opts = $.extend(defaults, options);
 		var months = Gantt.getMonths(opts.start, opts.end);
 		
@@ -23,7 +23,7 @@ cellWidth: number
 			Gantt.addHzHeader(div, months, opts.cellWidth);
 			Gantt.addVtHeader(div, opts.data);
 			Gantt.addGrid(div, opts.data, months, opts.cellWidth);
-			Gantt.addBlocks(div, opts.data, opts.cellWidth);
+			Gantt.addBlocks(div, opts.data, opts.cellWidth, opts.start);
 			
 			container.append(div);
 		});
@@ -98,8 +98,24 @@ cellWidth: number
 			div.append(gridDiv);
 		},
 		
-		addBlocks: function (dive, data, cellWidth) {
-			
+		addBlocks: function (div, data, cellWidth, start) {
+			var rows = $("div.ganttview-grid div.ganttview-grid-row", div);
+			var rowIdx = 0;
+			for (var i = 0; i < data.length; i++) {
+				for (var j = 0; j < data[i].series.length; j++) {
+					var size = DateUtils.daysBetween(data[i].series[j].start, data[i].series[j].end);
+					var offset = DateUtils.daysBetween(start, data[i].series[j].start);
+					var blockDiv = $("<div>", { 
+						"class": "ganttview-block", 
+						"css": { 
+							"width": (size * cellWidth) + "px",
+							"margin-left": (offset * cellWidth) + "px"
+						}
+					});
+					$(rows[rowIdx]).append(blockDiv);
+					rowIdx = rowIdx + 1;
+				}
+			}
 		}
 		
 	};
@@ -109,16 +125,16 @@ cellWidth: number
 var ArrayUtils = {
 	contains: function (arr, obj) {
 		var has = false;
-		for (var i = 0; i < arr.length; i++) {
-			if (arr[i] == obj) { has = true; }
-		}
+		for (var i = 0; i < arr.length; i++) { if (arr[i] == obj) { has = true; } }
 		return has;
 	}
 };
 
 var DateUtils = {
 	daysBetween: function (start, end) {
-		while ()
+		var count = 0;
+		while (start.compareTo(end) == -1) { count = count + 1; start.addDays(1); }
+		return count;
 	}
 };
 
@@ -127,26 +143,7 @@ var DateUtils = {
 /* SAMPLE GENERATED MARKUP
 
 <div class="ganttview">
-	
-	<div class="ganttview-hzheader">
-		<div class="ganttview-hzheader-months">
-			<div class="ganttview-hzheader-month">Jan</div>
-			<div class="ganttview-hzheader-month">Feb</div>
-			<div class="ganttview-hzheader-month">Mar</div>
-		</div>
-		<div class="ganttview-hzheader-days">
-			<div class="ganttview-hzheader-day">1</div>
-			<div class="ganttview-hzheader-day">2</div>
-			<div class="ganttview-hzheader-day">3</div>
-			<div class="ganttview-hzheader-day">1</div>
-			<div class="ganttview-hzheader-day">2</div>
-			<div class="ganttview-hzheader-day">3</div>
-			<div class="ganttview-hzheader-day">1</div>
-			<div class="ganttview-hzheader-day">2</div>
-			<div class="ganttview-hzheader-day">3</div>
-		</div>
-	</div>
-	
+
 	<div class="ganttview-vtheader">
 		<div class="ganttview-vtheader-item">
 			<div class="ganttview-vtheader-item-name">Feature 1</div>
@@ -164,51 +161,74 @@ var DateUtils = {
 		</div>
 	</div>
 	
-	<div class="ganttview-grid">
-		<div class="ganttview-grid-row">
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
+	<div class="ganttview-slider">
+	
+		<div class="ganttview-hzheader">
+			<div class="ganttview-hzheader-months">
+				<div class="ganttview-hzheader-month">Jan</div>
+				<div class="ganttview-hzheader-month">Feb</div>
+				<div class="ganttview-hzheader-month">Mar</div>
+			</div>
+			<div class="ganttview-hzheader-days">
+				<div class="ganttview-hzheader-day">1</div>
+				<div class="ganttview-hzheader-day">2</div>
+				<div class="ganttview-hzheader-day">3</div>
+				<div class="ganttview-hzheader-day">1</div>
+				<div class="ganttview-hzheader-day">2</div>
+				<div class="ganttview-hzheader-day">3</div>
+				<div class="ganttview-hzheader-day">1</div>
+				<div class="ganttview-hzheader-day">2</div>
+				<div class="ganttview-hzheader-day">3</div>
+			</div>
 		</div>
-		<div class="ganttview-grid-row">
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
+	
+		<div class="ganttview-grid">
+			<div class="ganttview-grid-row">
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+			</div>
+			<div class="ganttview-grid-row">
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+			</div>
+			<div class="ganttview-grid-row">
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+			</div>
+			<div class="ganttview-grid-row">
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+				<div class="ganttview-grid-row-cell"></div>
+			</div>
 		</div>
-		<div class="ganttview-grid-row">
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-		</div>
-		<div class="ganttview-grid-row">
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-			<div class="ganttview-grid-row-cell"></div>
-		</div>
+	
 	</div>
 	
 </div>
